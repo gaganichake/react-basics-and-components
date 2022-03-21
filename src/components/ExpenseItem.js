406ae0d402de14c3a3bc7cad3c6006b38d05d119
Gagan Ichake <gaganichake@yahoo.com>
2022-03-21 15:28:02 -0500

    Passing Data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,23 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   // You must have only ONE root HTML element in return statement as JSX expressions must have one parent element.
   // For multiple line you should wrape the whole HTML code (the JSX) inside braces "()" to signal JavaScript that it's a one statement
   // Use keyboard shortcut shift + option + F to format the source code in this document
   return (
-    <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
-      <div className='expense-item__description'>
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
