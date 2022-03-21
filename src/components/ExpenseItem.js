f98b2ec4aefc7dca972e916800f82c3d4bc35ecb
Gagan Ichake <gaganichake@yahoo.com>
2022-03-21 11:45:39 -0500

    Adding Basic CSS Styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,20 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   // You must have only ONE root HTML element in return statement as JSX expressions must have one parent element.
-  // For multiple line you should wrape the whole HTML code inside () to signal JavaScript that it's a one statement
+  // For multiple line you should wrape the whole HTML code (the JSX) inside braces "()" to signal JavaScript that it's a one statement
   // Use keyboard shortcut shift + option + F to format the source code in this document
   return (
-    <div>
+    <div className='expense-item'>
       <div>Date</div>
-      <div>
+      <div className='expense-item__description'>
         <h2>Title</h2>
-        <div>Amount</div>
+        <div className='expense-item__price'>Amount</div>
       </div>
       <div>March 21th 2022</div>
-      <div>
+      <div className='expense-item__description'>
         <h2>Car Insurance</h2>
-        <div>$345.90</div>
+        <div className='expense-item__price'>$345.90</div>
       </div>
     </div>
   );
