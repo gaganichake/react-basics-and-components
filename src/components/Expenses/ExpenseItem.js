79502834e6cc685ffb8b5562832980d7ba33b1ca
Gagan Ichake <gaganichake@yahoo.com>
2022-03-21 22:39:41 -0500

    An Alternative Function Syntax

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,7 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "./../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   // You must have only ONE root HTML element in return statement as JSX expressions must have one parent element.
   // For multiple line you should wrape the whole HTML code (the JSX) inside braces "()" to signal JavaScript that it's a one statement
   // Use keyboard shortcut shift + option + F to format the source code in this document
