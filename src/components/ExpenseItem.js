7737bff31883b3785cfc4116f15c1dd9a32172bf
Gagan Ichake <gaganichake@yahoo.com>
2022-03-21 22:03:38 -0500

    The Concept of Composition (children props)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   // You must have only ONE root HTML element in return statement as JSX expressions must have one parent element.
   // For multiple line you should wrape the whole HTML code (the JSX) inside braces "()" to signal JavaScript that it's a one statement
   // Use keyboard shortcut shift + option + F to format the source code in this document
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
